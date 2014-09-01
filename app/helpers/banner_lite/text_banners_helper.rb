35fe1101c987d3c3f354ef898ba987e5046b7d26
Satoshi Kobayashi <satoshi.forest@gmail.com>
2014-09-01 23:29:25 +0900

    add preview action for text_banner

--- app/helpers/banner_lite/text_banners_helper.rb
+++ app/helpers/banner_lite/text_banners_helper.rb
@@ -1,4 +1,12 @@
 module BannerLite
   module TextBannersHelper
+    def text_banners(bnnrs = nil)
+      bnnrs ||= TextBanner.active
+      bnnrs.map{|b| banner_html(b) }.join("\n").html_safe
+    end
+
+    def banner_html(banner)
+      link_to banner.banner_text, banner.url
+    end
   end
 end
